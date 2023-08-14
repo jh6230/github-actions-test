b14dde91868bd6d3c66d76446dfd9b14e1d2b812
Honda.Jun <j.honda.0326@gmail.com>
2023-08-14 11:51:07 +0000

    edit main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,18 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "192.0.0.0/22"
-  
+
   tags = {
     Name = "github-actions-test"
   }
-  
+
+}
+
+resource "aws_subnet" "my-vpc-subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "192.0.1.0/26"
+  tags = {
+    Name = "my-vpc-subnet"
+  }
 }
