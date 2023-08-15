db154269ec8a5ab8b31c8e2c326f1f230019aea1
“Xjh6230 <j.honda.0326@gmail.com>
2023-08-15 07:38:01 +0000

    create s3

--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
+# terraform {
+#   required_version = ">= 1.5.0"
+#   backend "s3" {
+#     bucket = "example-terraform-state" # 作成したS3バケット
+#     region = "ap-northeast"
+#     key = "terraform.tfstate"
+#     encrypt = true
+#   }
+# }
+
 provider "aws" {
   region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "github-actions-test-2023-0815"
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_vpc" "my_vpc" {
