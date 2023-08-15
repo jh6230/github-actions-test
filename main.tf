e447f9b7294165f9be4f060008d8197d5453c424
“Xjh6230 <j.honda.0326@gmail.com>
2023-08-15 08:09:32 +0000

    fix

--- main.tf
+++ main.tf
@@ -7,32 +7,32 @@ resource "aws_s3_bucket" "terraform_state" {
 
 terraform {
   required_version = ">= 1.5.0"
-  backend "s3" {
-    bucket = "github-actions-test-2023-0815" # 作成したS3バケット
-    region = "ap-northeast-1"
-    key = "terraform.tfstate"
-    encrypt = true
-  }
+  # backend "s3" {
+  #   bucket = "github-actions-test-2023-0815" # 作成したS3バケット
+  #   region = "ap-northeast-1"
+  #   key = "terraform.tfstate"
+  #   encrypt = true
+  # }
 }
 
-provider "aws" {
-  region = "ap-northeast-1"
-}
+# provider "aws" {
+#   region = "ap-northeast-1"
+# }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "192.0.0.0/22"
+# resource "aws_vpc" "my_vpc" {
+#   cidr_block = "192.0.0.0/22"
 
-  tags = {
-    Name = "github-actions-test"
-  }
+#   tags = {
+#     Name = "github-actions-test"
+#   }
 
-}
+# }
 
-resource "aws_subnet" "my-vpc-subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  availability_zone = "ap-northeast-1a"
-  cidr_block        = "192.0.1.0/26"
-  tags = {
-    Name = "my-vpc-subnet"
-  }
-}
+# resource "aws_subnet" "my-vpc-subnet" {
+#   vpc_id            = aws_vpc.my_vpc.id
+#   availability_zone = "ap-northeast-1a"
+#   cidr_block        = "192.0.1.0/26"
+#   tags = {
+#     Name = "my-vpc-subnet"
+#   }
+# }
