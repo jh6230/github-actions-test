2cf1bb53724005ea3178c69e8eaed1aa43e649c8
“Xjh6230 <j.honda.0326@gmail.com>
2023-08-15 08:16:40 +0000

    fix

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "github-actions-test-2023-0815"
-  versioning {
-    enabled = false
-  }
-}
-
 terraform {
   required_version = ">= 1.5.0"
   backend "s3" {
@@ -19,20 +12,20 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# resource "aws_vpc" "my_vpc" {
-#   cidr_block = "192.0.0.0/22"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "192.0.0.0/22"
 
-#   tags = {
-#     Name = "github-actions-test"
-#   }
+  tags = {
+    Name = "github-actions-test"
+  }
 
-# }
+}
 
-# resource "aws_subnet" "my-vpc-subnet" {
-#   vpc_id            = aws_vpc.my_vpc.id
-#   availability_zone = "ap-northeast-1a"
-#   cidr_block        = "192.0.1.0/26"
-#   tags = {
-#     Name = "my-vpc-subnet"
-#   }
-# }
+resource "aws_subnet" "my-vpc-subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "192.0.1.0/26"
+  tags = {
+    Name = "my-vpc-subnet"
+  }
+}
